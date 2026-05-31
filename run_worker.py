2ef8869aa24f63cbf45d424dfa9b4181b7be60f7
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-05-31 16:14:03 +0200

    Create run_worker.py

---
+++ run_worker.py
@@ -0,0 +1,16 @@
+import redis
+from rq import Worker, Queue, Connection
+
+from config import REDIS_URL
+
+
+listen = ['default']
+
+conn = redis.from_url(REDIS_URL)
+
+
+if __name__ == '__main__':
+
+    with Connection(conn):
+        worker = Worker(list(map(Queue, listen)))
+        worker.work()
